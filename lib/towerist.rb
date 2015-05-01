473a73aab06d497c6cf15ab4bc675e3d3b512192
zgs225 <zgs225@gmail.com>
2015-05-01 08:52:49 +0800

    换一种方式引入文件

--- lib/towerist.rb
+++
@@ -1,5 +0,0 @@
-# encoding: utf-8
-module Towerist
-  autoload Version, 'towerist/version'
-  autoload DataSource, 'towerist/data_source'
-end
--- lib/towerist/version.rb
+++ lib/towerist/version.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 
 module Towerist
-  module Version
+  class Version
     MAJOR = 1
     MINOR = 0
     PATCH = 0
